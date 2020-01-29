344256d1e6aedbd3073c9ad2244e9c31992431ce
Chris Ashman <chrisashman96@gmail.com>
2020-01-29 22:24:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,7 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {} 
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash
 end
 
 def changing_alan
@@ -106,6 +107,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
      
